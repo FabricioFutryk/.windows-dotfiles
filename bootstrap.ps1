80512b77c6ce4b7768aebf5d907991534d35fb7f
Belseir <ffutryk52@gmail.com>
2024-01-25 20:44:55 -0300

    fix: change zip extraction path & overwrite if already exists

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,10 +6,12 @@ Invoke-WebRequest `
 
 Expand-Archive `
   -Path "$HOME\Downloads\windows-dotfiles.zip" `
-  -DestinationPath "$env:TEMP"
+  -DestinationPath "$HOME" `
+  -Force
 
 Rename-Item `
   -NewName ".windows-dotfiles" `
-  -Path "$HOME\.windows-dotfiles-main"
+  -Path "$HOME\.windows-dotfiles-main" `
+  -Force
 
 Invoke-Expression "$HOME\.windows-dotfiles\Installation.ps1"
