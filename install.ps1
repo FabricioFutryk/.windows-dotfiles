de21c4e7aadf863826e4e8dc3e805f5ec78fabba
Belseir <ffutryk52@gmail.com>
2024-01-26 03:23:21 -0300

    refactor: move download functions to external module

--- install.ps1
+++ install.ps1
@@ -1,44 +1,4 @@
-function DownloadAndExtract {
-  param(
-    [string]$Url,
-    [string]$OutFile,
-    [string]$DestinationPath
-  )
-
-  Invoke-WebRequest `
-    -Uri $Url `
-    -OutFile $OutFile
-
-  Expand-Archive `
-    -Path $OutFile `
-    -DestinationPath $DestinationPath `
-    -Force
-
-  Remove-Item $OutFile -Force
-}
-
-function DownloadAndExecute {
-  param(
-    [string]$Url,
-    [string]$OutFile,
-    [string[]]$ArgumentList
-  )
-
-  Invoke-WebRequest `
-    -Uri $Url `
-    -OutFile $OutFile
-  
-  if($OutFile -match '\.cmd$') {
-    Start-Process `
-      -FilePath $OutFile `
-      -ArgumentList $ArgumentList `
-      -Wait
-  } else {
-    Invoke-Expression "$OutFile $ArgumentList"
-  }
-
-  Remove-Item $OutFile -Force
-}
+﻿Import-Module "$HOME\.windows-dotfiles\utils.ps1"
 
 # .gitconfig values
 
@@ -55,6 +15,7 @@ $gitConfig | Set-Content ".gitconfig"
 ##################################################################
 ## Windows Activation (massgravel/Microsoft-Activation-Scripts) ##
 ##################################################################
+
 
 $hwidActivatorURL = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/Separate-Files-Version/Activators/HWID_Activation.cmd'
 
---
+++ utils.ps1
@@ -0,0 +1,41 @@
+﻿function DownloadAndExtract {
+  param(
+    [string]$Url,
+    [string]$OutFile,
+    [string]$DestinationPath
+  )
+
+  Invoke-WebRequest `
+    -Uri $Url `
+    -OutFile $OutFile
+
+  Expand-Archive `
+    -Path $OutFile `
+    -DestinationPath $DestinationPath `
+    -Force
+
+  Remove-Item $OutFile -Force
+}
+
+function DownloadAndExecute {
+  param(
+    [string]$Url,
+    [string]$OutFile,
+    [string[]]$ArgumentList
+  )
+
+  Invoke-WebRequest `
+    -Uri $Url `
+    -OutFile $OutFile
+  
+  if($OutFile -match '\.cmd$') {
+    Start-Process `
+      -FilePath $OutFile `
+      -ArgumentList $ArgumentList `
+      -Wait
+  } else {
+    Invoke-Expression "$OutFile $ArgumentList"
+  }
+
+  Remove-Item $OutFile -Force
+}
